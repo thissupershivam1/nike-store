fed8586a7aa9715f647a95caf31eacafa8da71a8
shivam <shivammaur59@gmail.com>
2023-11-07 18:53:02 +0530

    final commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,43 @@
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8  z-50 w-full bg-black fixed '>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={headerLogo}
+            alt='logo'
+            width={129}
+            height={29}
+            className='m-0 w-[129px] h-[29px]'
+          />
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-white'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='flex gap-2 text-lg text-white-400 leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
